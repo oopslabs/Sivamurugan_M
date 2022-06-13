2e344481d5c7482dfc640503e54179563e582178
Sivamurugan <94527948+Siva-SMR@users.noreply.github.com>
2022-06-13 09:19:50 +0530

    Create CO2_Pg3.java

---
+++ CO2_Pg3.java
@@ -0,0 +1,16 @@
+public class strfn
+{
+
+	public static void main(String[] args)
+	{
+		String s="    Ajil kannan Arun siva    ";
+		System.out.println("UpperCase : "+s.toUpperCase());
+		System.out.println("LowerCase : "+s.toLowerCase());
+		System.out.println("Trim : "+s.trim());
+		System.out.println("Replace : "+s.replace("Arun","Uday"));
+		System.out.println("Length : "+s.length());
+		System.out.println("Character at position : "+s.charAt(9));
+
+	}
+
+}
