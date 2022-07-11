918870989ab42a6bdf27aeb911762969d56c7ec5
Sivamurugan <94527948+Siva-SMR@users.noreply.github.com>
2022-07-11 22:53:16 +0530

    Add files via upload

---
+++ Graphics/rectangle.java
@@ -0,0 +1,10 @@
+package Graphics;
+
+interface rect{
+	public void area(int len,int bre);
+}
+public class rectangle implements rect {
+	public void area(int leng,int bre) {
+		System.out.println("Area of rectangle :"+leng*bre);
+	}
+}
