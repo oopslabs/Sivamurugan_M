14807c01d44d01f34b609831a7f39529f43e5d1c
Sivamurugan <94527948+Siva-SMR@users.noreply.github.com>
2022-07-14 23:15:25 +0530

    Create CO5_Pg8.java

---
+++ CO5_Pg8.java
@@ -0,0 +1,26 @@
+import java.awt.event.*;
+import java.applet.*;
+import java.awt.*;
+/*<applet code="co5_pg8" width=300
+height=300></applet>*/
+public class co5_pg8 extends Applet implements KeyListener{
+    String msg=" ";
+    int x=30,y=50;
+    public void init(){
+        addKeyListener(this);
+        requestFocus();
+    }
+    public void keyTyped(KeyEvent ke){
+        msg+=ke.getKeyChar();
+        repaint();
+    }
+    public void keyReleased(KeyEvent ke){
+        showStatus("Key Up!");
+    }
+    public void keyPressed(KeyEvent ke){
+        showStatus("Key Down!");
+    }
+    public void paint(Graphics G){
+        G.drawString(msg,x,y);
+    }
+}
