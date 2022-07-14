800f934d399d14c28d9372baf64fcd85c846a1a1
Sivamurugan <94527948+Siva-SMR@users.noreply.github.com>
2022-07-14 23:10:22 +0530

    Create CO5_Pg1.java

---
+++ CO5_Pg1.java
@@ -0,0 +1,11 @@
+import java.applet.*; 
+import java.awt.Graphics;  
+public class co5_pg1 extends Applet
+{ 
+ 	public void paint(Graphics g){ 
+		g.drawOval(20, 20, 150, 150);  
+		g.drawRect(20, 200, 280, 40);
+		g.drawLine(20, 270, 300, 270); 
+	}   
+} 
+ /*<applet height="700" code="co5_pg1.class" width="500" border="2"></applet>*/
